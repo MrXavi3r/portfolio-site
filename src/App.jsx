d7193370cc3a1f0f3c7e3a8e98f3e041479f481c
Xavier Ritch <xritch89@hotmail.com>
2021-08-20 16:48:36 -0400

    menu working

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Works from "./components/works/Works";
 import Portfolio from "./components/portfolio/Portfolio";
 import Testimonials from "./components/testimonials/Testimonials";
 import Contact from "./components/contact/Contact";
+import Menu from "./components/menu/Menu";
 import "./app.scss";
 import { useState } from "react";
 function App() {
@@ -12,6 +13,7 @@ function App() {
   return (
     <div className="app">
       <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
       <div className="sections">
         <Intro />
         <Portfolio />

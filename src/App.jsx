ec1c7f91fba65bff643eb49b3bdb913ee28b784c
Xavier Ritch <xritch89@hotmail.com>
2021-08-20 14:53:43 -0400

    css, navbar menu toggle

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import Topbar from "./components/topbar/Topbar";
-import Intro from './components/intro/Intro'
+import Intro from "./components/intro/Intro";
 import Works from "./components/works/Works";
 import Portfolio from "./components/portfolio/Portfolio";
 import Testimonials from "./components/testimonials/Testimonials";
 import Contact from "./components/contact/Contact";
+import "./app.scss";
+import { useState } from "react";
 function App() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <div className="app">
-      <Topbar />
+      <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
       <div className="sections">
         <Intro />
         <Portfolio />
--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,9 +1,31 @@
-import './topbar.scss'
+import "./topbar.scss";
+import { Person, Mail } from "@material-ui/icons";
 
-export default function Topbar() {
-    return (
-        <div className="topbar">
-            
+export default function Topbar({ setMenuOpen, menuOpen }) {
+  return (
+    <div className={"topbar " + (menuOpen && "active")}>
+      <div className="wrapper">
+        <div className="left">
+          <a href="#intro" className="logo">
+            Xavier
+          </a>
+          <div className="itemContainer">
+            <Person className="icon" />
+            <span>954-296-6361</span>
+          </div>
+          <div className="itemContainer">
+            <Mail className="icon" />
+            <span>xritch89@hotmail.com</span>
+          </div>
         </div>
-    )
+        <div className="right">
+          <div className="hamburger" onClick={()=> setMenuOpen(!menuOpen)}>
+            <span className="line1"></span>
+            <span className="line2"></span>
+            <span className="line3"></span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }

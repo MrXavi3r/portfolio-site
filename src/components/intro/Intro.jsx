3b474b2aac9746069260033aac0478ade71e7ab6
Xavier Ritch <xritch89@hotmail.com>
2021-08-21 09:44:28 -0400

    started portfolio

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,9 +1,38 @@
-import './intro.scss'
+import "./intro.scss";
+import { init } from "ityped";
+import { useRef, useEffect } from "react";
 
 export default function Intro() {
-    return (
-        <div className="intro" id="intro">
-            intro
+  const textRef = useRef();
+
+  useEffect(() => {
+    init(textRef.current, {
+      showCursor: true,
+      backDelay: 1500,
+      backSpeed: 60,
+      strings: ["Developer", "Engineer", "Enthusiast"],
+    });
+  }, []);
+
+  return (
+    <div className="intro" id="intro">
+      <div className="left">
+        <div className="imgContainer">
+          <img src="assets/portrait.png" alt="xavier" />
         </div>
-    )
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>hi there, I'm</h2>
+          <h1>Xavier</h1>
+          <h3>
+            Software <span ref={textRef}></span>
+          </h3>
+        </div>
+        <a href="#portfolio">
+          <img src="assets/down.png" alt="down arrow" />
+        </a>
+      </div>
+    </div>
+  );
 }

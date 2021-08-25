435c7662f9cca74789a397ef30c8920e0df17e48
Xavier Ritch <xritch89@hotmail.com>
2021-08-24 22:49:10 -0400

    about me section updated

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -14,7 +14,11 @@ export default function About() {
               </p>
             </section>
             <section className="ltb">
-              <img src="assets/programmer.svg" alt="xavier" className="xavier" />
+              <img
+                src="assets/programmer.svg"
+                alt="xavier"
+                className="xavier"
+              />
             </section>
           </div>
           <div className="left-center">
@@ -27,7 +31,7 @@ export default function About() {
             <section className="lcb">
               <p>
                 I've been involved in financial markets as a trader for just
-                over a decade now... when im not coding, I spend much of my time
+                over a decade now. When im not coding, I spend much of my time
                 online analyzing charts, managing trades, and reading financial
                 news articles
               </p>
@@ -49,9 +53,54 @@ export default function About() {
           </div>
         </div>
         <div className="page right">
-          <div className="right-top"></div>
-          <div className="right-center"></div>
-          <div className="right-bottom"></div>
+          <div className="right-top">
+            <h2>TECH STACKS I'M FAMILIAR WITH...</h2>
+            <p>
+              I keep an open mind to any language, library, or framework, as I
+              recognize that each is simply a tool for which to achieve an end.
+              Its always best to choose the best tools for the job. Though with
+              that being said, focus is important, and for that reason I work
+              primarily in Javascript and with the React library. On the back
+              end, I'm also familiar with Node.js and the Express framework.
+              CSS, Sass, and Bootstrap for styling, and have some limited
+              experience with MongoDB as well as PostgreSQL.
+            </p>
+          </div>
+          <div className="right-bottom">
+            <section className="icons-container">
+              <img src="assets/javascript.svg" alt="javascript" />
+              <img src="assets/react.svg" alt="javascript" />
+              <img src="assets/mongodb.svg" alt="javascript" />
+              <img src="assets/sass.svg" alt="javascript" />
+              <img src="assets/bootstrap.svg" alt="javascript" />
+              <img src="assets/css.svg" alt="javascript" />
+              <img src="assets/nodejs.svg" alt="javascript" />
+              <img src="assets/express-js.svg" alt="javascript" />
+              <img src="assets/postgresql.svg" alt="javascript" />
+            </section>
+            <section className="connect-container">
+              <a
+                href="https://www.linkedin.com/in/xavier-ritch-254238145/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <div className="linkedin card">
+                  <img src="assets/linkedin.svg" alt="linkedin" />
+                  <span>my LinkedIn</span>
+                </div>
+              </a>
+              <a
+                href="https://github.com/MrXavi3r"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <div className="github card">
+                  <img src="assets/github.svg" alt="github" />
+                  <span>my Github</span>
+                </div>
+              </a>
+            </section>
+          </div>
         </div>
       </div>
     </div>

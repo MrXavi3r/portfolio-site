d1c16e40320d9c68770d9b78e9c888130b5b436f
Xavier Ritch <xritch89@hotmail.com>
2021-09-12 15:13:14 -0400

    slider component functional

---
+++ src/components/works/SliderComponent.jsx
@@ -0,0 +1,47 @@
+import Slider from "react-slick";
+import { data } from "../../data/worksData";
+import './slider.scss'
+
+
+const settings = {
+  dots: true,
+  infinite: true,
+  speed: 500,
+  adaptiveHeight: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 5000,
+  fade: false
+};
+
+export const SliderComponent = () => {
+  return (
+    <Slider {...settings}>
+      {data.map((d) => {
+          return (
+            <div className="show-container" key={d.id}>
+              <div className="item">
+                <div className="left">
+                  <div className="leftContainer">
+                    <div className="imgContainer">
+                      <img src={d.icon} alt={d.title} />
+                    </div>
+                    <h2>{d.title}</h2>
+                    <p>{d.desc}</p>
+                    <a href="/"><span>view on Github</span></a>
+                  </div>
+                </div>
+                <div className="right">
+                  <img
+                    src={d.img}
+                    alt={d.title}
+                  />
+                </div>
+              </div>
+            </div>
+          );
+        })}
+    </Slider>
+  );
+};
--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -1,59 +1,13 @@
 import "./works.scss";
-import { useState } from "react";
-import { data } from "../../data/worksData";
+import { SliderComponent } from "./SliderComponent";
 
 export default function Works() {
-  const [currentSlide, setCurrentSlide] = useState(0);
-  const handleClick = (direction) => {
-    direction === "left"
-      ? setCurrentSlide(currentSlide > 0 ? currentSlide - 1 : 2)
-      : setCurrentSlide(currentSlide < data.length - 1 ? currentSlide + 1 : 0);
-  };
-
   return (
     <div className="works" id="works">
-    <h2 className="works-heading">Showcase</h2>
-      <div
-        className="slider"
-        style={{ transform: `translateX(-${currentSlide * 100}vw)` }}
-      >
-        {data.map((d) => {
-          return (
-            <div className="container" key={d.id}>
-              <div className="item">
-                <div className="left">
-                  <div className="leftContainer">
-                    <div className="imgContainer">
-                      <img src={d.icon} alt={d.title} />
-                    </div>
-                    <h2>{d.title}</h2>
-                    <p>{d.desc}</p>
-                    <span>view on Github</span>
-                  </div>
-                </div>
-                <div className="right">
-                  <img
-                    src={d.img}
-                    alt={d.title}
-                  />
-                </div>
-              </div>
-            </div>
-          );
-        })}
+      <h2 className="works-heading">Showcase Project</h2>
+      <div className="slider-container">
+        <SliderComponent />
       </div>
-      <img
-        src="assets/arrow.png"
-        alt="arrow"
-        className="arrow left"
-        onClick={() => handleClick("left")}
-      />
-      <img
-        src="assets/arrow.png"
-        alt="arrow"
-        className="arrow right"
-        onClick={() => handleClick("right")}
-      />
     </div>
   );
 }

dff6d58913b0087c5b89285e543cddcb6888ff4c
Xavier Ritch <xritch89@hotmail.com>
2021-09-30 23:25:10 -0400

    updated about me section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -4,58 +4,34 @@ import diver from "../../diver.svg";
 
 export default function About() {
   return (
-    <div className="about" id="about">
-      <h1>A BIT ABOUT ME...</h1>
-      <div className="top">
-        <div
-          className="top-left"
-          style={{
-            backgroundImage: `url(${diver})`,
-            backgroundRepeat: "no-repeat",
-            backgroundPosition: "center",
-          }}
-        >
-          <img src="assets/bitcoin.svg" alt="bitcoin" />
-        </div>
-        <div className="top-right">
-          <p className="forex">
-            I've been involved in the financial markets as a trader for just
-            over a decade now. When i'm not coding, I spend much of my time
-            online analyzing charts, managing trades, and reading financial news
-            articles. <mark>PRO TIP:</mark> buy and hold onto some bitcoin!
-          </p>
-          <p>
-            Despite the time I spend online, I'm a big outdoors guy. I love
-            playing sports, running at the track and pickup basketball. I love
-            to explore. Skydiving is on my bucket list, and scuba diving is my
-            favorite hobby. I go whenever possible.
-          </p>
-        </div>
+    <section className="about" id="about">
+      <div className="left">
+        <img
+          src="assets/about_img.svg"
+          alt="about"
+          width="600"
+          className="about-img"
+        />
       </div>
-      <div className="bottom">
-        <div className="bottom-left">
-        </div>
-        <div className="bottom-right">
-        <p className="growth">
-            Growth and impact are what I personally strive for on a daily basis.
-            My goals are to continue to improve myself personally and level up
-            my skills, while positively impacting those who I come in touch
-            with.
-          </p>
-          <p>
-            I'm eager to work on challenging projects alongside other talented
-            developers and i'm curious about the Dev Ops end of things, from
-            taking an app from the inception of the idea, through development,
-            to deployment and continuous integration.
-          </p>
-          {/* <div>JAVASCRIPT</div>
-          <div>REACT</div>
-          <div>CSS/SASS</div>
-          <div>MONGODB</div>
-          <div>NODE/EXPRESS</div>
-          <div>GIT/GITHUB</div> */}
-        </div>
+      <div className="right">
+        <p>
+          Growth and impact are what I personally strive for on a daily basis.
+          My goals are to continue to improve myself personally and level up my
+          skills, while positively impacting those who I come in touch with.
+          I'm eager to work on challenging projects alongside other talented
+          developers and i'm curious about the Dev Ops end of things, from
+          taking an app from the inception of the idea, through development, to
+          deployment and continuous integration.
+          I've been involved in the financial markets as a trader for just over
+          a decade now. When i'm not coding, I spend much of my time online
+          analyzing charts, managing trades, and reading financial news
+          articles. <mark>PRO TIP:</mark> buy and hold onto some bitcoin!
+          Despite the time I spend online, I'm a big outdoors guy. I love
+          playing sports, running at the track and pickup basketball. I love to
+          explore. Skydiving is on my bucket list, and scuba diving is my
+          favorite hobby. I go whenever possible.
+        </p>
       </div>
-    </div>
+    </section>
   );
 }
--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -12,10 +12,43 @@ export default function Skills() {
         </p>
       </div>
       <div className="skill-container">
-        <div className="skill-item">1</div>
-        <div className="skill-item">2</div>
-        <div className="skill-item">3</div>
-        <div className="skill-item">4</div>
+        <div className="skill-item item-1">
+          <img src="assets/react-2.svg" alt="react-logo" />
+          <h4>Powerful Frontend Technologies</h4>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero
+            unde reiciendis impedit dignissimos fugit qui cumque molestiae
+            consectetur ratione quas, repudiandae ea exercitationem autem
+            minima!
+          </p>
+        </div>
+        <div className="skill-item item-2">
+          <img src="assets/nodejs.svg" alt="node.js logo" />
+          <h4>Seamless Backend Integration</h4>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus
+            officia tenetur et doloremque ipsam expedita quam nemo asperiores.
+            Quia vel saepe placeat, suscipit dolorum labore!
+          </p>
+        </div>
+        <div className="skill-item item-3">
+          <img src="assets/sass.svg" alt="sass logo" />
+          <h4>Attractive Design</h4>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus
+            officia tenetur et doloremque ipsam expedita quam nemo asperiores.
+            Quia vel saepe placeat, suscipit dolorum labore!
+          </p>
+        </div>
+        <div className="skill-item item-4">
+          <img src="assets/mongodb.svg" alt="mongoDB logo" />
+          <h4>Convenient Database Solutions</h4>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus
+            officia tenetur et doloremque ipsam expedita quam nemo asperiores.
+            Quia vel saepe placeat, suscipit dolorum labore!
+          </p>
+        </div>
       </div>
     </div>
   );
